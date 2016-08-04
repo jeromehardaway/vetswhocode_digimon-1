168c87a327b51aa822d07add101507e48a2c0025
John Garcia <johngarcia9110@gmail.com>
2016-08-04 02:22:56 +0000

    commit class files for 8_3

---
+++ vwc_digimon/8_3/class_initialize.rb
@@ -0,0 +1,14 @@
+class Hello
+    def initialize (name)
+        @name = name
+    end
+    
+    def hello
+        puts "Hello, " + @name + "!"
+    end
+    
+end
+
+=begin
+use irb to show how this works
+=end
