316574e085f333f5be6e165696b69ba5f252493b
John Garcia <johngarcia9110@gmail.com>
2016-08-05 01:21:26 +0000

    Commit for class on 8_4

---
+++ vwc_digimon/8_4/class_method.rb
@@ -0,0 +1,7 @@
+class Area
+    def Area.rect(length, width, units = "inches")
+        area = length * width
+        printf( "The area of this rectangle is %.2f %s.", area, units)
+             
+    end
+end
---
+++ vwc_digimon/8_4/inheritance.rb
@@ -0,0 +1,9 @@
+class Name
+    
+    print "John"
+
+end
+
+class FirstName < Name
+   print "Garcia \n" 
+end
