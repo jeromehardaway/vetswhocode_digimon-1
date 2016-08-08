2dbd9a413c3f16249ac4657a47ed810e8974f3e1
John Garcia <johngarcia9110@gmail.com>
2016-08-08 01:59:55 +0000

    PragStudio ex10 in ex7

--- Pragmatic Studios/studio_game/studio_game_ex7.rb
+++ Pragmatic Studios/studio_game/studio_game_ex7.rb
@@ -30,6 +30,32 @@ class Player
     end
 end
 
+class Game
+   def initialize(title)
+       @title = title
+       @players = []
+    end
+    
+    attr_reader :title
+    
+    def add_play(a_player)
+        @players << a_player
+    end
+    
+    def play
+        puts "There are #{@players.size} players in #{@title}: "
+        @players.each do |player|
+            puts player
+        end
+        @players.each do |player|
+           player.blam
+           player.wOOt
+           puts player
+        end
+    end
+    
+end
+
 player1 = Player.new("Moe", 100)
 player1.wOOt
 puts player1
